829edfbbbb1649d531de418348855893f1b96f99
Tra-Ta <madtazik@gmail.com>
2022-12-23 21:29:22 +0300

    Create programm

---
+++ Programm/Program.cs
@@ -0,0 +1,51 @@
+﻿string[] FillArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите значения {i} индекса массива -> ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"‟{array[i]}‟,");
+        else Console.Write($"‟{array[i]}‟");
+    }
+    Console.Write("]");
+}
+string[] FilterArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+
+    string[] newArray = new string[count];
+    int k = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[k] = array[i];
+            k++;
+        }
+    }
+    return newArray;
+}
+Console.Write("Введите размер массива -> ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+if (sizeArray < 0) Console.WriteLine("Вы ввели некорректный размер массива");
+else
+{
+    string[] array = FillArray(sizeArray);
+    PrintArray(array);
+    string[] arrayNew = FilterArray(array);
+    Console.Write(" -> ");
+    PrintArray(arrayNew);
+}
